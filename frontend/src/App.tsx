f1616bdf2af98e0fa2abceb7a9633472b01b38bd
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2025-07-11 02:30:47 +0530

    added inline-padding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 	return (
 		<div className="min-h-screen bg-background">
 			<Navigation />
-			<main>
+			<main className="px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
 				<Hero />
 				<About />
 				<Projects />
